8b8c50294fdc040cd791cb2eba65c538f8424d7c
Marie Pierard <pierardmarie@gmail.com>
2020-07-10 12:35:16 +0200

    fin série 3

--- 11.Javascript/01.Series-1/03-colors/01-change-bcg-one/script.js
+++ 11.Javascript/01.Series-1/03-colors/01-change-bcg-one/script.js
@@ -3,7 +3,19 @@
 
 
 (() => {
+    document.getElementById("red").addEventListener("click", function() {
+    document.body.style.backgroundColor = "red"; 
+    });
 
-    // your code here
+    document.getElementById("green").addEventListener("click", function() {
+    document.body.style.backgroundColor = "green"; 
+    });
 
+    document.getElementById("yellow").addEventListener("click", function() {
+    document.body.style.backgroundColor = "yellow"; 
+    });
+
+    document.getElementById("blue").addEventListener("click", function() {
+    document.body.style.backgroundColor = "blue"; 
+    });
 })();
--- 11.Javascript/01.Series-1/03-colors/03-change-bcg-three/script.js
+++ 11.Javascript/01.Series-1/03-colors/03-change-bcg-three/script.js
@@ -5,5 +5,9 @@
 (() => {
 
     // your code here
-
+    document.getElementById("run").addEventListener("click", function () {
+        /*voir https://css-tricks.com/snippets/javascript/random-hex-color/*/
+        let color = Math.floor(Math.random()*16777215).toString(16);
+        document.body.style.backgroundColor = "#" + color;
+    });
 })();
